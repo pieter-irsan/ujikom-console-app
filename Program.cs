2d102dadc4fab431119a8a2e0dfb49b81918fde7
pieter-irsan <pieterirsan.pi@gmail.com>
2021-12-22 18:33:54 +0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Praktek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu menu = new Menu();
+            menu.Option();
+        }
+    }
+    
+    class Menu
+    {
+        public void Option()
+        {
+            Console.WriteLine("\n=============================");
+            Console.WriteLine("PT ICL ABADI");
+            Console.WriteLine("=============================");
+            Console.WriteLine("[1] Calculate costs");
+            Console.WriteLine("[2] See previous costs");
+            Console.WriteLine("[3] Exit");
+
+            string option = "";
+            Method method = new Method();
+            option = Console.ReadLine();
+            switch (option)
+            {
+                case "1":
+                    method.Result();
+                    break;
+                case "2":
+                    method.GetCosts();
+                    break;
+                case "3":
+                    Console.WriteLine(">Exiting program with 'return;'");
+                    return;
+                default:
+                    Console.WriteLine("Choose from the available option!");
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadKey(true);
+                    Option();
+                    break;
+                }
+        }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,7 @@
+Praktek UJIKOM C#
+- Pieter Irsan Hutagaol
+
+Alur Program
+--> Menu.Option()
+----> Calculation.Result()
+------> Method class untuk menjalankan fungsi dan save ke database
